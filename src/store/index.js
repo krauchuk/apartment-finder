13d63fd98031cc163f1ab55f07b9d10747be4fc8
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2022-11-22 16:43:23 +0100

    removed redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
-import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
+
+import asyncMiddleware from './middlewares/async'
 
 import rootReducer from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export default createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+export default createStore(rootReducer, composeEnhancers(applyMiddleware(asyncMiddleware)))
