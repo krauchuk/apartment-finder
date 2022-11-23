eac83e1ab951add53f0ebc2ce5f765c4183f8c12
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2022-11-23 19:49:57 +0100

    removed logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 
 import asyncMiddleware from './middleware/async'
-import loggerMiddleware from './middleware/logger'
 
 import rootReducer from './reducers'
 
 const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 const composeEnhancers = devtools || compose
 
-const middleware = [asyncMiddleware]
-if (!devtools) middleware.push(loggerMiddleware)
-
-export default createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)))
+export default createStore(rootReducer, composeEnhancers(applyMiddleware(asyncMiddleware)))
