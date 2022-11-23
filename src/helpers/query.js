39100b0aa86ab10fe4a96e9815f90f11b0b46b19
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2022-11-23 16:46:51 +0100

    upd query helper

--- src/helpers/query.js
+++ src/helpers/query.js
@@ -4,3 +4,12 @@ export const toObject = string =>
 
     return { ...acc, [name]: value }
   }, {})
+
+export const toString = object =>
+  Object.keys(object)
+    .map(key => `${key}=${object[key]}`)
+    .join('&')
+
+export const getLikeString = () => window.location.search.slice(1)
+
+export const getLikeObject = () => toObject(getLikeString())
