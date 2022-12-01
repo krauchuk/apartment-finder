440ca58be7cd6cf0c9b586a30646b98c50be7be3
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2022-12-01 21:13:46 +0100

    added main info to ad page

--- src/pages/Ad/index.jsx
+++ src/pages/Ad/index.jsx
@@ -6,11 +6,21 @@ import actionTypes from '@actions'
 
 import Spinner from '@components/common/Spinner'
 
-import { Wrapper } from './styles'
+import { Wrapper, AdHeader, AdName, AdAddress, AdPrice, MainInfo, MainInfoBlock, MainInfoRow } from './styles'
+
+const Row = ({ title, info, children }) => (
+  <MainInfoRow>
+    <span>{title}</span>
+    <span>
+      {info}
+      {children}
+    </span>
+  </MainInfoRow>
+)
 
 const AdPage = () => {
   const { id } = useParams()
-  const { selected, loading } = useSelector(state => state.ads)
+  const { selected: data, loading } = useSelector(state => state.ads)
 
   const dispatch = useDispatch()
 
@@ -25,7 +35,30 @@ const AdPage = () => {
       </Wrapper>
     )
 
-  return <Wrapper>{selected.id}</Wrapper>
+  return (
+    <Wrapper>
+      <AdHeader>
+        <AdName>{data.name}</AdName>
+        <AdPrice>${data.price}</AdPrice>
+        <AdAddress>
+          {data.city}, {data.address}
+        </AdAddress>
+      </AdHeader>
+      <MainInfo>
+        <MainInfoBlock>
+          <Row title="Rooms" info={data.rooms} />
+          <Row title="Square" info={data.square}>
+            m<sup>2</sup>
+          </Row>
+          <Row title="Floor" info={data.floor} />
+        </MainInfoBlock>
+        <MainInfoBlock>
+          <Row title="Construction year" info={data.constructionYear} />
+          <Row title="Wall material" info={data.wallMaterial} />
+        </MainInfoBlock>
+      </MainInfo>
+    </Wrapper>
+  )
 }
 
 export default AdPage
--- src/pages/Ad/styles.js
+++ src/pages/Ad/styles.js
@@ -1,3 +1,60 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  max-width: 1000px;
+  margin: auto;
+  padding: 16px;
+  & > :not(:last-child) {
+    margin-bottom: 16px;
+  }
+`
+
+export const AdHeader = styled.div`
+  width: 100%;
+  display: inline-grid;
+  grid-template-columns: 1fr auto;
+  & > :not(:last-child) {
+    margin-bottom: 8px;
+  }
+`
+
+export const AdName = styled.span``
+
+export const AdAddress = styled.span`
+  color: #2185d0;
+`
+
+export const AdPrice = styled.span`
+  font-weight: bold;
+  font-size: 20px;
+`
+
+export const MainInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const MainInfoBlock = styled.div`
+  flex: 1;
+  &:first-child {
+    margin-right: 16px;
+  }
+`
+
+export const MainInfoRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 4px 4px;
+  border-bottom: 1px solid #dededf;
+  &:first-child {
+    border-top: 1px solid #dededf;
+  }
+  & > span {
+    flex: 1;
+  }
+  & > :last-child {
+    font-weight: bold;
+  }
+`
