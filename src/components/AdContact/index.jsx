1e8518f67b48ba6dc975460f3d31bac092f016f1
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2022-12-20 19:21:16 +0100

    removed StarRating

--- src/components/AdContact/index.jsx
+++ src/components/AdContact/index.jsx
@@ -3,14 +3,6 @@ import { useSelector } from 'react-redux'
 
 import { Wrapper, RatingBlock, Phone, Email, Rating, Star } from './styles'
 
-const StarRating = ({ rating }) => (
-  <Rating>
-    {[1, 2, 3, 4, 5].map(r => (
-      <Star key={r} active={r <= rating} />
-    ))}
-  </Rating>
-)
-
 const AdContact = () => {
   const info = useSelector(state => state.ads.selected.contact) || {}
 
@@ -19,7 +11,12 @@ const AdContact = () => {
       <h3>{info.name}</h3>
       {info.type === 'company' && (
         <RatingBlock>
-          <StarRating rating={info.rating} /> {info.votes} vote(s)
+          <Rating>
+            {[1, 2, 3, 4, 5].map(r => (
+              <Star key={r} active={r <= info.rating} />
+            ))}
+          </Rating>{' '}
+          {info.votes} vote(s)
         </RatingBlock>
       )}
       <Phone href={`tel:${info.phone}`}>Make a phone call</Phone>
